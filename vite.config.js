acd7efd9afb8792aedc697119f1c0ab4137c9672
dellson-arch <nayanmahato908@gmail.com>
2025-07-25 23:01:33 -0700

    final-7

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import path from "path"
-import tailwindcss from "@tailwindcss/vite" // Import tailwindcss plugin
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()], // Include tailwindcss plugin here
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
